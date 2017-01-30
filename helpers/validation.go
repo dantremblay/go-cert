2af052a208adfd0e05912d70e1a9deda484684eb
Julien Kassar <github@kassisol.com>
2017-01-30 13:43:15 -0500

    Add some helpers validations

---
+++ helpers/validation.go
@@ -0,0 +1,62 @@
+package helpers
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/juliengk/go-utils"
+)
+
+var (
+	NotValidCACommonName = fmt.Errorf("common name cannot contains the words \"root\", \"intermediate\" and \"ca\"")
+	NotValidCAType       = fmt.Errorf("ca type is not valid")
+	NotValidCAOU         = fmt.Errorf("organizational unit cannot contains the words \"certificate\" and \"authority\"")
+)
+
+func IsValidCACommonName(cn string) error {
+	blacklisted := []string{
+		"Root",
+		"root",
+		"Intermediate",
+		"intermediate",
+		"CA",
+		"ca",
+	}
+
+	words := strings.Split(cn, " ")
+
+	for _, word := range words {
+		if utils.StringInSlice(word, blacklisted) {
+			return NotValidCACommonName
+		}
+	}
+
+	return nil
+}
+
+func IsValidCAType(catype string) error {
+	if catype != "root" && catype != "intermediate" {
+		return NotValidCAType
+	}
+
+	return nil
+}
+
+func IsValidCAOrgUnit(ou string) error {
+	blacklisted := []string{
+		"Certificate",
+		"certificate",
+		"Authority",
+		"authority",
+	}
+
+	words := strings.Split(ou, " ")
+
+	for _, word := range words {
+		if utils.StringInSlice(word, blacklisted) {
+			return NotValidCAOU
+		}
+	}
+
+	return nil
+}
